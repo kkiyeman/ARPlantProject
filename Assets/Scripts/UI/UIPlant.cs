438ac5a362aa0a568989d1c4e8ddeae669c4fbf0
DESKTOP-QVOJ483\iibi203 <lojugt98@gmail.com>
2023-01-18 12:51:19 +0900

    UIPlant 연결 및 버튼스크립트 추가

--- Assets/Scripts/UI/UIPlant.cs
+++ Assets/Scripts/UI/UIPlant.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 
 public class UIPlant : MonoBehaviour
@@ -22,8 +23,9 @@ public class UIPlant : MonoBehaviour
     [SerializeField] Text txtCurtime;
     [SerializeField] Text txtEnergy;
     [SerializeField] Text txtGold;
-    [HideInInspector] public int energy;
-    [HideInInspector] public int Gold;
+    [HideInInspector] public int totalEnergy = 100;
+    [HideInInspector] public int curEnergy=50;
+    [HideInInspector] public int Gold = 1000;
     [SerializeField] Slider sldEnergy;
 
     [Header("Status")]
@@ -31,6 +33,7 @@ public class UIPlant : MonoBehaviour
     [SerializeField] Button btnCloseStatus;
     [SerializeField] Button btnPlantName;
     [SerializeField] Text txtPlantName;
+    string plantName = "FishBone";
     [SerializeField] InputField inputPlantName;
     [SerializeField] Button btnClosePlantInputfield;
     [SerializeField] Image imgsWhatPlant;
@@ -45,11 +48,65 @@ public class UIPlant : MonoBehaviour
     [SerializeField] Image imgSick;
     [SerializeField] Image imgThirsty;
 
+    [Header("Test")]
+    [SerializeField] Button btnSamplePlant;
+
 
     void Start()
     {
-        
+        ButtonSetting();
     }
 
+    void Update()
+    {
+        TextSetting();
+    }
+
+    private void TextSetting()
+    {
+        txtPlantName.text = plantName;
+        txtCurtime.text = DateTime.Now.ToString();
+        txtEnergy.text = $"{curEnergy}/{totalEnergy}";
+        txtGold.text = $"{Gold}";
+    }
+    private void ButtonSetting()
+    {
+        btnSamplePlant.onClick.AddListener(OnClickStatusOn);
+        btnCloseStatus.onClick.AddListener(OnClickStatusOff);
+        btnMemo.onClick.AddListener(OnClickMemoOn);
+        btnCloseMemo.onClick.AddListener(OnClickMemoOff);
+        btnPlantName.onClick.AddListener(OnClickInputPlantNameOn);
+        btnClosePlantInputfield.onClick.AddListener(OnClickInputPlantNameOff);
+    }
+
+    private void OnClickStatusOn()
+    {
+        imgStatus.gameObject.SetActive(true);
+    }
+
+    private void OnClickStatusOff()
+    {
+        imgStatus.gameObject.SetActive(false);
+    }
+
+    private void OnClickMemoOn()
+    {
+        imgMemo.gameObject.SetActive(true);
+    }
+
+    private void OnClickMemoOff()
+    {
+        imgMemo.gameObject.SetActive(false);
+    }
+
+    private void OnClickInputPlantNameOn()
+    {
+        inputPlantName.gameObject.SetActive(true);
+    }
+
+    private void OnClickInputPlantNameOff()
+    {
+        inputPlantName.gameObject.SetActive(false);
+    }
 
 }
