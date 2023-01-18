aa111dc8fe4557b41cdfd1e213f21e9e5d32b2c8
DESKTOP-QVOJ483\iibi203 <lojugt98@gmail.com>
2023-01-18 11:57:27 +0900

    UIPlant 프리펩,스크립트 수정

--- Assets/Scripts/UI/UIPlant.cs
+++ Assets/Scripts/UI/UIPlant.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class UIPlant : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Right Side")]
+    [SerializeField] Button btnTemp;
+    [SerializeField] Button btnStore;
+
+    [Header("Bottom Side")]
+    [SerializeField] GameObject bottomButtons;
+    [SerializeField] Button btnWater;
+    [SerializeField] Button btnPill;
+    [SerializeField] Button btnComment;
+    [SerializeField] Button btnSeed;
+
+    [Header("Upper Side")]
+    [SerializeField] Image weather;
+    [SerializeField] Text txtCurtime;
+    [SerializeField] Text txtEnergy;
+    [SerializeField] Text txtGold;
+    [HideInInspector] public int energy;
+    [HideInInspector] public int Gold;
+    [SerializeField] Slider sldEnergy;
+
+    [Header("Status")]
+    [SerializeField] Image imgStatus;
+    [SerializeField] Button btnCloseStatus;
+    [SerializeField] Button btnPlantName;
+    [SerializeField] Text txtPlantName;
+    [SerializeField] InputField inputPlantName;
+    [SerializeField] Button btnClosePlantInputfield;
+    [SerializeField] Image imgsWhatPlant;
+    [SerializeField] Button btnMemo;
+    [SerializeField] Image imgMemo;
+    [SerializeField] Text txtMemo;
+    [SerializeField] Button btnCloseMemo;
+    [SerializeField] InputField inputMemo;
+    [SerializeField] Slider sldGrowth;
+    [SerializeField] Slider sldNutrition;
+    [SerializeField] Slider sldHumidity;
+    [SerializeField] Image imgSick;
+    [SerializeField] Image imgThirsty;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
