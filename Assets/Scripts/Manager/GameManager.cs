b0f79c41579d252013e7b6f137a4014d564cc707
DESKTOP-QVOJ483\iibi203 <mnbvcxz195@naver.com>
2023-01-26 11:02:21 +0900

    에너지 회복 함수 수정

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -20,24 +20,29 @@ public class GameManager : MonoBehaviour
     #endregion
 
     [HideInInspector] public int totalEnergy = 100;     //전체 에너지
-    [HideInInspector] public int curEnergy = 100;             //현재 에너지
+     public int curEnergy = 10;             //현재 에너지
 
-    [HideInInspector] public int runningTime;         //어플 진행 시간
+    public int runningTime;         //어플 진행 시간
 
     public int curPoint = 0;                               //게임 내 포인트
     public int curGameMoney = 0;                           //게임 머니
 
     public bool isEnegyZero;                            //에너지 제로 판별 여부
 
+    public WaitForSecondsRealtime waitFor20Seconds = new WaitForSecondsRealtime(20);
+
+    private void Start()
+    {
+        StartCoroutine("PlusEnergy");
+    }
 
     private void Update()
     {
         runningTime = (int)Time.realtimeSinceStartup;
-        PlusCurEnergy();
         CurEnergyZero();
     }
 
-    public void PlusCurEnergy()                    //5분마다 에너지 채워지는 함수(5분마다 1씩)
+/*    public void PlusCurEnergy()                    //5분마다 에너지 채워지는 함수(5분마다 1씩)
     {
         if (runningTime % 20 == 0)                 //에너지 채워지는 시간(일단은 20초로) 개발 완료후 300초로 변경
         {
@@ -52,6 +57,23 @@ public class GameManager : MonoBehaviour
         }
         else
             return;
+    }*/
+
+    IEnumerator PlusCurEnergy()                      //5분마다 에너지 채워지는 함수(5분마다 1씩)
+    {
+        while (true)
+        {
+            yield return waitFor20Seconds;       //에너지 채워지는 시간(일단은 20초로) 개발 완료후 300초로 변경
+
+            if (curEnergy > totalEnergy)
+            {
+                curEnergy = totalEnergy;
+            }
+            else
+            {
+                curEnergy += 1;
+            }
+        }
     }
 
     public void CurEnergyZero()              //에너지 제로시 물주기, 영양제, 수확, 칭찬하기 불가
@@ -62,7 +84,7 @@ public class GameManager : MonoBehaviour
             isEnegyZero = true;
         }
         else
-            return;
+            isEnegyZero = false;
     }
 
     public bool SpendGameMoney(int priceGM)   //현재 소지금과 가격을 비교해서 구매 가능 여부 판단 함수
