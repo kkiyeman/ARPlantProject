ac6a5da011ee8aeb838e551792395f284ec6c08b
DESKTOP-R01SK1J\용준 <lojugt98@gmail.com>
2023-01-30 07:13:24 +0900

    UIPlant, UIStore, UIDictionary 전체적 수정, UIDictionary 스크립트 추가 및 기능 추가

--- Assets/Scripts/UI/UIDictionary.cs
+++ Assets/Scripts/UI/UIDictionary.cs
@@ -1,9 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIDictionary : MonoBehaviour
 {
+    [Header("Standard Dictionary")]
+    [SerializeField] Button btnCloseDict;
+    [SerializeField] InputField inputSearch;
+    [SerializeField] Button btnSearch;
+    [SerializeField] Button btnPrev;
+    [SerializeField] Button btnNext;
+
+    [Header("Plant List")]
+    [SerializeField] Button[] btnPlantList;
+    [SerializeField] Text[] txtIPlantList;
+
+    [Header("Plant Detail")]
+    [SerializeField] GameObject plantDetail;
+    [SerializeField] Image imgPlant;
+    [SerializeField] Text txtPlantName;
+    [SerializeField] Text txtPlantKind;
+    [SerializeField] Text txtPlantClass;
+    [SerializeField] Text txtPlantInfo;
+    [SerializeField] Button btnCloseDetail;
+
+
+
     GameManager gamemanager;
     PlantManager plantmanager;
     UIManager uimanager;
@@ -13,11 +36,44 @@ public class UIDictionary : MonoBehaviour
         gamemanager = GameManager.GetInstance();
         plantmanager = PlantManager.GetInstance();
         uimanager = UIManager.GetInstance();
+        SetButton();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SetButton()
+    {
+        btnCloseDict.onClick.AddListener(OnClickCloseDict);
+        btnCloseDetail.onClick.AddListener(OnClickCloseDetail);
+        OnClickDetail();
+    }
+
+
+    private void OnClickCloseDict()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnClickCloseDetail()
+    {
+        plantDetail.gameObject.SetActive(false);
+    }
+
+    private void OnClickDetail()
+    {
+        for(int i = 0; i<btnPlantList.Length; i++)
+        {
+            int idx = i;
+            btnPlantList[i].onClick.AddListener(OnClickDetailOpen);
+        }
+    }
+
+    private void OnClickDetailOpen()
+    {
+        plantDetail.gameObject.SetActive(true);
     }
 }
