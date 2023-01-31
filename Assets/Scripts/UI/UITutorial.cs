f234254f1d2ea9c83dfe19d32753fa2dfb426f36
DESKTOP-QVOJ483\iibi203 <jseong444@naver.com>
2023-01-31 18:55:33 +0900

    옵션 UI 수정

--- Assets/Scripts/UI/UITutorial.cs
+++ Assets/Scripts/UI/UITutorial.cs
@@ -7,138 +7,99 @@ using UnityEngine.UI;
 
 public class UITutorial : MonoBehaviour
 {
-    public Button bottomOnbtn;
-    public Button topOnbtn;
-    public Button rightOnbtn;
-    public Button WatertutoBtn;
-    public Button NutritutoBtn;
-    public Button CommenttutoBtn;
-    public Button SeedtutoBtn;
+    [Header("띄울 영역")]
+    [SerializeField] GameObject bottomSide;
+    [SerializeField] GameObject topSide;
+    [SerializeField] GameObject rightSide;
+    [SerializeField] GameObject status;
 
-    public GameObject bottomSide;
-    public Image topSide;
-    public Image rightSide;
-    public Image status;
-    public Image AlphaBg;
-    public Image[] AlphaBgList;
-    public Image BgImg;
+    [Header("포문")]
+    [SerializeField] Image[] Arrows;
+    [SerializeField] Button[] BottomBtnList;
 
-    public Button[] BottomBtnList;
-    public Button WaterBtn;
-    public Button NextBtn;
+    [Header("중앙")]
+    [SerializeField] Button NextBtn;
+    [SerializeField] Text Dirtxt;
+    [SerializeField] Text Skiptxt;
 
-    public Text Dirtxt;
-
-    public AudioSource[] NarList;
-    public AudioSource Nar1;
-
-    public AudioClip[] NarclipList;
-    public AudioClip NarClip1;
-    public AudioClip NarClip2;
-
-    public int TutorialStep;
+    int index;
 
     // Start is called before the first frame update
     void Start()
     {
-        bottomOnbtn.onClick.AddListener(BottomTutoBtn);
-        rightOnbtn.onClick.AddListener(RightTutoBtn);
-        topOnbtn.onClick.AddListener(TopTutoBtn);
-        WaterBtn.onClick.AddListener(WaterBtnOn);
-
-        StartCoroutine(TutorialStart());
-        TutorialStep = 0;
-    }
-    IEnumerator TutorialStart()
-    {
-        Step1();
-        yield return new WaitWhile(() => TutorialStep < 1);
-        Step2();
-        yield return new WaitWhile(() => TutorialStep < 2);
-        Step3();
-        yield return new WaitWhile(() => TutorialStep < 3);
-
-
-    }
-    void Step1()
-    {
-
+        Skiptxt.text = "다음";
+        bottomSide.gameObject.SetActive(false);
+        rightSide.gameObject.SetActive(false);
+        topSide.gameObject.SetActive(false);
         Dirtxt.text = "화면 아래 부분은 식물 성장 관련 버튼들입니다.";
-        NextBtn.gameObject.SetActive(true);
-        bottomSide.gameObject.SetActive(true);
+        NextBtn.onClick.AddListener(BottomTutoBtn);
+        /*rightOnbtn.onClick.AddListener(RightTutoBtn);
+        topOnbtn.onClick.AddListener(TopTutoBtn);*/
 
-/*        for (int i = 0; i < BottomBtnList.Length; i++)
-        {
-            int index = i;
-            // BottomBtnList[index].gameObject.AddComponent<AudioSource>();
-            BottomBtnList[index].onClick.AddListener(() => this.Btuto(index));
-            Debug.Log("버튼 셋팅 완료");
-        }*/
-    }
-    void Step2()
-    { 
-    
-    }
-    void Step3()
-    {
 
+        /*StartCoroutine(TutorialStart());*/
     }
 
     void BottomTutoBtn()
     {
+        Dirtxt.text = "버튼을 눌러보세요!";
+        Step1();
+    }
+
+    void Step1()
+    {
+
         
-    }
-    void RightTutoBtn()
-    {
-        rightSide.gameObject.SetActive(true);  
-        topSide.gameObject.SetActive(false);
-        bottomSide.gameObject.SetActive(false);
-    }
-    void TopTutoBtn()
-    {
-        topSide.gameObject.SetActive(true);
-        bottomSide.gameObject.SetActive(false);
-        rightSide.gameObject.SetActive(false);
-    }
-    IEnumerator BottomTuto(int index)
-    {
-        Nar1.clip = Resources.Load<AudioClip>($"NarClip{index}");
-        Nar1.Play();
-        while (true)
-        {
-            yield return new WaitForSeconds(1.0f);
-            if (!Nar1.isPlaying)
+        /*NextBtn.gameObject.SetActive(true);*/
+        bottomSide.gameObject.SetActive(true);
+        
+        for (int i = 0; i < BottomBtnList.Length; i++)
             {
-                WatertutoBtn.gameObject.SetActive(true);
-                bottomSide.gameObject.SetActive(false);
-                WaterBtn.gameObject.SetActive(true);
-                Dirtxt.text = "버튼을 눌러보세요!";
-                BottomBtnList[index].gameObject.SetActive(false);
+                int index = i;
+                BottomBtnList[index].gameObject.AddComponent<AudioSource>();
 
-
-                /*Nar1.clip = NarClip2;
-                Nar1.Play();
-                Nar1.loop = true; */
-
+            BottomBtnList[index].onClick.AddListener(() => { SelectedArrow(index); });
+            BottomBtnList[index].onClick.AddListener(() => { SetBtn(index); });
+                Debug.Log("버튼 셋팅 완료");
+                
             }
 
-        }
+        
     }
-    void WaterBtnOn()
-    { 
-        WaterBtn.gameObject.SetActive(false);
+    void SelectedArrow(int num)
+    {
+        Arrows[index].gameObject.SetActive(false);
+        Arrows[num].gameObject.SetActive(true);
+        index = num;
 
     }
-    void Btuto(int index)
+    void RightTutoBtnOn()
     {
-        StartCoroutine("BottomTuto");
-        
-        string[,] AnswertxtList = new string[,]{
-        { "보호자와 약속되지 않은 곳은 가면 안돼요." , "-어딘가 가고 싶을때는 부모님이나 보호자에게, 가는 위치와 이유를 설명해야해요." , ""}, /*1행*/
-        { "신호등의 초록불이 깜빡일때 무리해서 건너면 안돼요.","-안전하게 다음 신호를 기다렸다, 꼭 도로의 좌우를 확인하고 손을 들고 횡단보도를 건너가야해요.",""}, /*2행*/
-        { "모르는 사람을 함부로 도와주면 안돼요","-모르는 사람이 주는 음식을 먹거나 모르는 사람의 차를 타면 안돼요.","-어른들은 어린이들에게 도와달라고 하지 않아요.누군가 도움을 청할땐 그 주변의 어른에게 도와달라고 전달하세요."}, /*3행*/
-        { "공사장 주변은 다가가면 안돼요.","-공사장 위에서 물건이 떨어지거나 공사장 시설이 무너질 수도 있어요.","-공사장이 지름길이더라도 안전하게 다른 길로 돌아가요."}, /*4행*/
-        { "아무리 잘 아는 어른이어도 혼자서 따라가면 안돼요.","-잘 아는 어른이어도 혼자 따라가면 위험해요.","-혹시 가고 싶다면 부모님이나 보호자에게 어디 가는지 알린뒤 허락을 받고 가야해요."} /*5행*/
-    };
+        bottomSide.gameObject.SetActive(false);
+        rightSide.gameObject.SetActive(true);
+
+    }
+    void SetBtn(int index)
+    {
+        Skiptxt.text = "건너뛰기";
+        NextBtn.onClick.RemoveAllListeners();
+        NextBtn.onClick.AddListener(RightTutoBtnOn);
+        string[] txtList = new string[]
+        { "물뿌리개를 이용해 식물에 물을 줄 수 있어요. 수분도가 너무 높거나 낮으면 식물이 죽으니 주의하세요!" ,
+        "영양제를 이용해 식물에 영양을 줄 수 있어요. 영양도가 너무 높거나 낮으면 식물이 질병에 걸립니다.",
+        "칭찬은 하루에 1회 가능합니다. 칭찬받은 식물은 영양도와 수분도를 조금 올려줍니다.",
+        "화분에 씨앗을 심을 수 있습니다. 씨앗은 농작물과 관상용으로 나뉩니다. 원하는 씨앗을 심어보세요!" };
+
+        string v = txtList[index].ToString();
+        Dirtxt.text = v;
+
+        string w = txtList[index].ToString();
+        Dirtxt.text = w;
+
+        string y = txtList[index].ToString();
+        Dirtxt.text = y;
+
+        string z = txtList[index].ToString();
+        Dirtxt.text = z;
     }
 }   
