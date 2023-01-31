76c936b1e4a27dc48c603a4db1ac8e39cce15502
DESKTOP-R01SK1J\용준 <lojugt98@gmail.com>
2023-02-01 02:31:22 +0900

    UIDictionary 수정중

--- Assets/Scripts/UI/UIDictionary.cs
+++ Assets/Scripts/UI/UIDictionary.cs
@@ -24,6 +24,9 @@ public class UIDictionary : MonoBehaviour
     [SerializeField] Text txtPlantClass;
     [SerializeField] Text txtPlantInfo;
     [SerializeField] Button btnCloseDetail;
+    private string[] plantInfo = new string[6];
+    private string[] plantClass = new string[6];
+    private string[] plantKind = new string[6];
 
 
 
@@ -37,12 +40,35 @@ public class UIDictionary : MonoBehaviour
         plantmanager = PlantManager.GetInstance();
         uimanager = UIManager.GetInstance();
         SetButton();
+        InitPlantInfo();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void InitPlantInfo()
+    {
+        plantInfo[0] = "알로에는 크산토로이아과 알로에속의 여러해살이 다육식물로 노회, 나무노회라고도 하는데, 노회는 알로에의 '로에'를 음차한 것이다. 중동, 마다가스카르, 아프리카 남부가 원산지로 약 200여 종류가 있다. 이집트·그리스·로마에서는 기원전부터 재배되고 있었다고 한다. 종자 또는 꺾꽂이로 번식하며 소형종은 분재된다. 남북아메리카, 유럽에서는 알로에베라로도 알려진 바베이도스알로에를 재배하는데 이 종류들은 흔히 볼 수 있는 원예 식물이기도 하다.";
+        plantInfo[1] = "여인초는 여인초의 유일종이며 마다가스카르에 서식한다. 이 속은 남아프리카의 극락조화속, 남아메리카의 좁은부채파초속과 근연속이다. 몇몇 오래된 분류체계에서는 이 속들을 바나나가 속해있는 파초과에 분류시키기도 한다. 여인초속은 보통 단일종으로 여겨지지만, 그동안 네 가지의 형태가 식별되었다.";
+        plantInfo[2] = "";
+        plantInfo[3] = "";
+        plantInfo[4] = "";
+        plantInfo[5] = "";
+        plantClass[0] = "비짜루목";
+        plantClass[1] = "생강목";
+        plantClass[2] = "카리오필라레스목";
+        plantClass[3] = "";
+        plantClass[4] = "";
+        plantClass[5] = "";
+        plantKind[0] = "크산토로이아과";
+        plantKind[1] = "극락조화과";
+        plantKind[2] = "선인장과";
+        plantKind[3] = "";
+        plantKind[4] = "";
+        plantKind[5] = "";
     }
 
     private void SetButton()
