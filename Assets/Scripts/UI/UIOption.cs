642a734c43b4480371082894e43158097644dfb7
DESKTOP-QVOJ483\iibi203 <jseong444@naver.com>
2023-02-02 16:07:59 +0900

    옵션 최종완성

--- Assets/Scripts/UI/UIOption.cs
+++ Assets/Scripts/UI/UIOption.cs
@@ -7,12 +7,21 @@ using UnityEngine.UI;
 
 public class UIOption : MonoBehaviour
 {
+    public string[,] HelpList = new string[,]{
+        { "식물 심기" , "식물은 화분에 씨앗을 심으면 자라납니다. 씨앗을 상점에서 구매해 화분에 심고 이름을 지어보세요.","(1/5)"}, /*1행*/
+        { "식물 기르기","식물에게 물을 주고, 영양제를 주고 칭찬을 해줄 수 있습니다. 영양도가 0이하이거나, 수분도가 너무 많거나 적으면 식물이 죽어요.","(2/5)"}, /*2행*/
+        { "식물 상태이상","식물의 수분이 너무 많거나, 영양도가 너무 낮거나 높으면 식물의 상태이상이 생깁니다. 식물이 노랗게 변했다면 상태창을 확인해보세요!","(3/5)"}, /*3행*/
+        { "식물 칭찬하기","식물도 칭찬을 받으면 잘 자라난답니다. 하루에 1번 칭찬을 통해 수분도와 영양도를 획득하세요!","(4/5)"}, /*4행*/
+        { "식물 배치하기","나만의 식물을 원하는 위치로 옮겨보세요! 내 방이 나만의 정원으로 바뀌는 시간! 홈가드닝으로 함께 힐링해봐요~","(5/5)"} /*5행*/
+    };
+
+
     // 메인화면버튼
     public Button CloseBtn;
     public Button HelpBtn;
     public Button SoundBtn;
     public Button ExitBtn;
-    
+
     // 사운드화면
     public Button SoundBackBtn;
     public Slider Bgm;
@@ -20,13 +29,12 @@ public class UIOption : MonoBehaviour
 
     // 도움말화면
     public Button HelpBackBtn;
-    public Button[] buttons;
+    public Button NextBtn;
+    public Button PrevBtn;
     public Image ExampleImg;
     public Text HelpTitle;
     public Text HelpTxt;
     public Text HelpCount;
-    public string[,] HelpList;
-
 
     // 패널들
     public GameObject Option;
@@ -38,12 +46,14 @@ public class UIOption : MonoBehaviour
     AudioManager audioManager;
     public AudioMixer audioMixer;
 
+    int index = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
         audioManager = GetComponent<AudioManager>();
-        
+
 
         CloseBtn.onClick.AddListener(CloseOption);
         HelpBtn.onClick.AddListener(OpenHelp);
@@ -56,19 +66,11 @@ public class UIOption : MonoBehaviour
         Bgm.value = 0.5f;
         SFX.value = 0.5f;
 
-/*        if (buttons != null)
-        {
-            for (int i = 0; i < buttons.Length; i++)
-            {
-                int index = i;
-                buttons[index].gameObject.AddComponent<AudioSource>();
-                
+        NextBtn.onClick.AddListener(IndexPlus);
+        PrevBtn.onClick.AddListener(IndexMinus);
 
-                buttons[index].onClick.AddListener(() => this.ShowHelp(index));
 
-            }
-        }*/
-
+        PrevBtn.gameObject.SetActive(false);
     }
 
     void CloseOption()
@@ -105,7 +107,7 @@ public class UIOption : MonoBehaviour
     }
 
     void Exit()
-    { 
+    {
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
@@ -126,16 +128,10 @@ public class UIOption : MonoBehaviour
 
     public void ShowHelp(int index)
     {
-        string[,] HelpList = new string[,]{
-        { "식물 심기" , "식물은 화분에 씨앗을 심으면 자라납니다. 씨앗을 상점에서 구매해 화분에 심고 이름을 지어보세요.","(1/5)"}, /*1행*/
-        { "식물 기르기","식물에게 물을 주고, 영양제를 주고 칭찬을 해줄 수 있습니다. 영양도가 0이하이거나, 수분도가 너무 많거나 적으면 식물이 죽어요.","(2/5)"}, /*2행*/
-        { "식물 상태이상","식물의 수분이 너무 많거나, 영양도가 너무 낮거나 높으면 식물의 상태이상이 생깁니다. 식물이 노랗게 변했다면 상태창을 확인해보세요!","(3/5)"}, /*3행*/
-        { "식물 칭찬하기","식물도 칭찬을 받으면 잘 자라난답니다. 하루에 1번 칭찬을 통해 수분도와 영양도를 획득하세요!","(4/5)"}, /*4행*/
-        { "식물 배치하기","나만의 식물을 원하는 위치로 옮겨보세요! 내 방이 나만의 정원으로 바뀌는 시간! 홈가드닝으로 함께 힐링해봐요~","(5/5)"} /*5행*/
-    };
+        
         string v = HelpList[index, 0].ToString();
         HelpTitle.text = v;
-        
+
         //HelpTitle.gameObject.GetComponent<AudioSource>().Play();
 
         string y = HelpList[index, 1].ToString();
@@ -143,11 +139,42 @@ public class UIOption : MonoBehaviour
 
         string z = HelpList[index, 2].ToString();
         HelpCount.text = z;
-        
+
         ExampleImg.sprite = Resources.Load<Sprite>($"Image/Help/Help{index + 1}");
         Debug.Log($"{index}번째 이미지를 불러옵니다.");
         AudioManager.GetInstance().PlaySfx("뿅");
+        
+    }
 
+    private void IndexPlus()
+    {
+        index++;
+
+        int strLength = HelpList.GetLength(0);
+        if (index >= strLength)
+            index = strLength - 1;
+
+        if (index >= strLength - 1)
+            NextBtn.gameObject.SetActive(false);
+
+        PrevBtn.gameObject.SetActive(true); 
+        
+        ShowHelp(index);        
+    }
+
+    private void IndexMinus()
+    {
+        index--;
+
+        if (index <= 0)
+        {
+            index = 0;
+            PrevBtn.gameObject.SetActive(false);
+        }
+
+        NextBtn.gameObject.SetActive(true); 
+        
+        ShowHelp(index);        
     }
 
 }
