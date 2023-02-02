9cba838edd750b85554a0fcf9e3b450a1432ee21
DESKTOP-QVOJ483\iibi203 <jseong444@naver.com>
2023-02-02 17:08:09 +0900

    알림UI 초안

--- Assets/Scripts/UI/UINotice.cs
+++ Assets/Scripts/UI/UINotice.cs
@@ -16,50 +16,31 @@ public class UINotice : MonoBehaviour
     GameObject NoticePanal;
 
     public List<MyPlantList> myPlantList = new List<MyPlantList>();
-    int index;
     // Start is called before the first frame update
     void Start()
     {
       OkBtn.onClick.AddListener(OkNoticeBtn);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        CheckGrowPlant();
-        CheckDiePlant();
-    }
-
-    void CheckGrowPlant()
-    {
-       /* if (myPlantList[idx].growthRate >= 100)
-        {
-            GrowthNotice();
-            MyPlantList[idx].growthRate = 0;
-        }*/
-    }
-
-    void CheckDiePlant()
-    {
-        /*if (MyPlantManager.GetInstance().DieThePlant)
-        {
-            DieNotice();
-        }*/
-    }
-    public void DieNotice()
+    public void DieNotice(string name)
     {
         NoticePanal.SetActive(true);
-        Noticetxt.text = $"네가 없던 사이에 우리 {index}가 죽고 말았어..! 새로운 식물을 키우고 싶다면 씨앗을 다시 심어보자.";
+        Noticetxt.text = $"네가 없던 사이에 우리 {name}가 죽고 말았어..! 새로운 식물을 키우고 싶다면 씨앗을 다시 심어보자.";
     }
-    public void GrowthNotice()
+    public void GrowthNotice(string name)
     {
         rewardtxt.text = $"+{myPlantManager.GrowPlantReward}";
         rewardImg.sprite = Resources.Load<Sprite>("Icon/Store/Icon_Money");
         gameManager.curGameMoney = myPlantManager.GrowPlantReward++;
         NoticePanal.SetActive(true);
-        Noticetxt.text = $"와!{index}이 멋지게 성장했어! 앞으로 더욱 애정을 담아서 길러줘!";
+        Noticetxt.text = $"와!{name}이 멋지게 성장했어! 앞으로 더욱 애정을 담아서 길러줘!";
     }
-    void OkNoticeBtn()
+    public void DiseaseNotice(string name)
+    {
+        NoticePanal.SetActive(true);
+        Noticetxt.text = $"큰일이야! 우리 {name}가 아파..! 상태창을 확인해줘ㅠㅠ";
+    }
+    public void OkNoticeBtn()
     {
         NoticePanal.gameObject.SetActive(false);
     }
